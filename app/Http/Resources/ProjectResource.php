f6d318e7769360b3c7900727df3c1fc955823698
mohammad <mohammad@inktweb.nl>
2025-09-30 10:50:38 +0200

    Added resources for models

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'due_date' => Carbon::createFromDate($this->due_date)->format('d-m-Y'),
+            'status' => $this->status,
+            'image' => $this->image,
+            'created_by' => new UserResource($this->createdBy),
+            'updated_by' => new UserResource($this->updatedBy),
+            'created_at' => Carbon::createFromDate($this->created_at)->format('d-m-Y'),
+            'updated_at' => Carbon::createFromDate($this->updated_at)->format('d-m-Y'),
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email
+        ];
+    }
+}
