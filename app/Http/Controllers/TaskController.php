27c9499ba8c471b0c268142a3876028cd0a8b3b0
mohammad <mohammad@inktweb.nl>
2025-09-30 10:01:29 +0200

    Added resources controllers

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        //
+    }
+
+    public function show(Task $task)
+    {
+        //
+    }
+
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+    }
+
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
